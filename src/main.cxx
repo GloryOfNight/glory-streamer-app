be1c89eddaa48a8c570064ec3678943488f6dcde
Siarhei Dziki <siarhei.dziki@gmail.com>
2024-07-10 19:26:58 +0200

    moved back to sdl2 since sdl3 still a mess

--- src/main.cxx
+++ src/main.cxx
@@ -1,20 +1,20 @@
+#define SDL_MAIN_HANDLED
+
 #include "youtube_data_api.hxx"
 
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_hints.h>
-#include <SDL3/SDL_opengl.h>
-#include <codecvt>
-#include <format>
-#include <imgui.h>
-#include <imgui_impl_sdl3.h>
-#include <imgui_impl_sdlrenderer3.h>
+#include <SDL2/SDL.h>
+
 #include <iostream>
-#include <locale>
 #include <nlohmann/json.hpp>
 
-int main(int argc, char* argv[], char* envp[])
+int main(int argc, char* argv[])
 {
-	std::locale::global(std::locale("en_US.utf8"));
+	SDL_SetMainReady();
+	if (SDL_Init(SDL_INIT_VIDEO))
+	{
+		std::cerr << "Failed to initialize SDL" << SDL_GetError() << std::endl;
+		return -1;
+	}
 
 	const std::string clientId = "1002066649738-3gfrfvhfdt9q2j3n7vq1ufkdlav603a9.apps.googleusercontent.com";
 	const std::string clientSecret = "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";
@@ -37,5 +37,6 @@ int main(int argc, char* argv[], char* envp[])
 		titles.push_back(item["subscriberSnippet"]["title"]);
 	}
 
+	SDL_Quit();
 	return 0;
 }
